730e543b245f248ea6802702b089fec41df7367f
mcimam <imam123.ci@gmail.com>
2024-07-29 16:09:52 +0700

    [IMP] Change trombi shape

--- yo_trombinoscope_member/static/src/snippets/s_trombinoscope_member/000.js
+++ yo_trombinoscope_member/static/src/snippets/s_trombinoscope_member/000.js
@@ -13,22 +13,6 @@ const TrombinoscopeMember = publicWidget.Widget.extend({
     start() {
         this.loadImage();
     },
-    convertToSquareMatrix(arr, size) {
-        // Check if the array can form a perfect square matrix
-        while (size * size > arr.length) {
-            size -= 1
-        }
-
-        const matrix = [];
-        for (let i = 0; i < size; i++) {
-            matrix[i] = [];
-            for (let j = 0; j < size; j++) {
-                matrix[i][j] = arr[i * size + j];
-            }
-        }
-
-        return matrix;
-    },
     renderImgGrid(responses) {
         if (responses.length == 0) {
             return;
@@ -41,31 +25,39 @@ const TrombinoscopeMember = publicWidget.Widget.extend({
         }
 
         // Structure images to rectangle(TxT) grid
-        let colSize = parseInt(this.$target.attr('data-trombinoscope-size')) || '3';
-        let data = this.convertToSquareMatrix(responses, colSize)
+        const colSize = parseInt(this.$target.attr('data-trombinoscope-col')) || 1;
+        const rowSize = parseInt(this.$target.attr('data-trombinoscope-row')) || 1;
+        const data = responses
         let res = "";
-        let index = 0;
+
         if (data.length == 0) {
             console.warn("No trombinoscope image")
             return;
         }
 
-        // Determine coll class
-        data.forEach(row => {
-            res += `<div class="row trombinoscope-row gx-1 mb-2 justify-content-center">`;
-            row.forEach(col => {
-                const image = col.image
-                delete col.image
-                const raw_detail = col
-                const detail = JSON.stringify(col)
-                res += `
+        let index = 0;
+        for (let i = 0; i < rowSize; i++) {
+            let rowContent = `<div class="row trombinoscope-row gx-1 mb-2 justify-content-center">`;
+            let rowBreak = false;
+
+            for (let j = 0; j < colSize; j++) {
+                if (!data[index]) {
+                    rowBreak = true;
+                    break;
+                }
+
+                const colData = data[index];
+                const { image, ...rest } = colData;
+                const detail = JSON.stringify(rest);
+
+                rowContent += `
                     <div class="col-auto">
                         <div class="card trombinoscope-card" data-detail='${detail}'>
                             <a href="#" class="trombinoscope-card-link">
                                 <img src="data:image/png;base64,${image}" class="card-img-top img-thumbnail" alt="..."/>
                                 <div class="card-body trombinoscope-card-text">
                                     <h5 class="card-title">
-                                        ${col.name}
+                                        ${rest.name}
                                     </h5>
                                 </div>
                             </a>
@@ -73,10 +65,15 @@ const TrombinoscopeMember = publicWidget.Widget.extend({
                     </div>
                 `;
                 index += 1;
-            });
-            res += `</div>`;
-        });
+            }
 
+            rowContent += `</div>`;
+            res += rowContent;
+
+            if (rowBreak) {
+                break;
+            }
+        }
 
         gridElement.html(res);
 
@@ -121,7 +118,7 @@ const TrombinoscopeMember = publicWidget.Widget.extend({
 
         // Set Modal Body
         let detailElement = '';
-        for(const [key, value] of Object.entries(data)){
+        for (const [key, value] of Object.entries(data)) {
             const val = value != '' && value != null ? value : '-'
             detailElement += `
             <tr>
