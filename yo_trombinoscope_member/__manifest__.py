a1631576021d7b7d2c216517b84ea5e67d06ec5b
tn <tn>
2024-12-19 11:46:13 +0700

    return assets linkage for manifest, just in case

--- yo_trombinoscope_member/__manifest__.py
+++ yo_trombinoscope_member/__manifest__.py
@@ -17,5 +17,13 @@ Features including:
         'views/snippets/s_trombinoscope_member.xml',
         'views/snippets/snippets.xml',
     ],
-    # "assets" likely has been deprecated. assets are loaded by making ir.asset record
+    "assets": {
+        "website.assets_wysiwyg": [
+            "yo_trombinoscope_member/static/src/snippets/s_trombinoscope_member/options.js",
+        ],
+        "web.assets_frontend": [
+            "yo_trombinoscope_member/static/src/snippets/s_trombinoscope_member/000.js",
+            "yo_trombinoscope_member/static/src/snippets/s_trombinoscope_member/000.scss",
+        ],
+    },
 }
