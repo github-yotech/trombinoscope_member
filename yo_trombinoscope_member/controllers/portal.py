ae9ad25968526b684b1a3c7d50c03834f0c73ff6
Ubuntu <ubuntu@ns5005790.ip-51-79-229.net>
2025-04-16 09:45:10 +0200

    [FIX] migrate more code

--- yo_trombinoscope_member/controllers/portal.py
+++ yo_trombinoscope_member/controllers/portal.py
@@ -1,9 +1,9 @@
 from odoo.addons.portal.controllers.portal import CustomerPortal
 
 class PortalTrombinoscope(CustomerPortal):
-    
+    OPTIONAL_CAFEECO_FIELDS = ["description", "favorite_quote"]
+
     def _get_optional_fields(self) -> list[str]:
         res = super()._get_optional_fields()
-        if "description" not in res: res.append("description")
-        res = [*res, "favorite_quote"]
+        res = [*res, *self.OPTIONAL_CAFEECO_FIELDS]
         return res
